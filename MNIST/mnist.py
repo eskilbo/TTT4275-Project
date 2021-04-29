2def661076ba8eb478a4a06688ce365e42a69f66
eskilbo <47594779+eskilbo@users.noreply.github.com>
2021-04-29 10:28:43 +0200

    update MNIST clustering finished

--- MNIST/mnist.py
+++ MNIST/mnist.py
@@ -11,6 +11,7 @@ from pandas import DataFrame
 import matplotlib.pyplot as plt
 from PIL import Image
 from confusion_matrix_pretty_print import pretty_plot_confusion_matrix
+from scipy.spatial import distance
 import time
 
 def load_data(N_TRAIN, N_TEST):
@@ -28,13 +29,10 @@ def load_data(N_TRAIN, N_TEST):
     test_labels = np.frombuffer(test_labels[8:N_TEST+8], dtype=np.uint8)
     return train_img, train_labels, test_img, test_labels
 
-def diff_image(img1, img2):
+def euclid_distance(img1, img2):
     a = img1-img2
     b = np.uint8(img1<img2) * 254 + 1
-    return a * b
-
-def euclid_distance(img1, img2):
-    return np.sum(diff_image(img1, img2))
+    return np.sum(a * b)
 
 def nearest_neighbor_classifier(train_img, train_labels, test_img, test_labels, N_TRAIN, N_TEST):
     correct_pred = [] # Array of set of indeces of correct predictions
@@ -45,10 +43,11 @@ def nearest_neighbor_classifier(train_img, train_labels, test_img, test_labels, 
     for i in range(N_TEST):
         prediction = 0
         pred_img_index = 0
-        min = 65025*28*28*2
+        min = float('inf')
         # Comparing distance of test image to every training image
         for j in range(N_TRAIN):
-            d = euclid_distance(test_img[i], train_img[j])
+            #d = euclid_distance(test_img[i], train_img[j])
+            d = distance.euclidean(test_img[i], train_img[j])
             if d < min:
                 min = d
                 pred_img_index = j
@@ -62,14 +61,12 @@ def nearest_neighbor_classifier(train_img, train_labels, test_img, test_labels, 
     end = time.time()
     print(f"Runtime of program is {(end-start)/60} minutes.")
     df_cm = DataFrame(confusion_matrix, index=["0","1","2","3","4","5","6","7","8","9"], columns=["0","1","2","3","4","5","6","7","8","9"])
-    pretty_plot_confusion_matrix(df_cm,title='Confusion matrix - 1NN Classifier without clustering - 10000 test 60000 train',cmap="Oranges",pred_val_axis='x')
+    pretty_plot_confusion_matrix(df_cm,title='Confusion matrix - 1NN Classifier with clustering - 10000 test 640 clusters',cmap="Oranges",pred_val_axis='x')
     return 
 
 def clustering(data,data_lab, n_clusters):
     N = 10
-    data_class_len = data.shape[0]//N
-
-    #sorting out classes from dataset
+    # Sorting out classes from dataset
     templates_by_class = []
     for n in range(N):
         t = []
@@ -86,11 +83,8 @@ def clustering(data,data_lab, n_clusters):
         clusters.append(kmeans.cluster_centers_)
         labels.append([i]*n_clusters)
 
-
-    # flatten_list = [j for sub in labels for j in sub] 
     cluster_img = np.array(clusters).flatten().reshape((n_clusters*N,data.shape[1]))
     cluster_lab = np.array(labels,dtype=int).flatten().reshape(n_clusters*N,1)
-
     np.save('cluster_img.npy',cluster_img)
     np.save('cluster_lab.npy',cluster_lab)
     return cluster_img, cluster_lab
@@ -99,18 +93,19 @@ def clustering(data,data_lab, n_clusters):
 def main():
     N_TRAIN = 60000
     N_TEST = 10000
-    train_img, train_labels, test_img, test_labels = load_data(N_TRAIN, N_TEST)
-    # cluster_img, cluster_lab = clustering(train_img,train_labels,64)
-    cluster_img = np.load('cluster_img.npy')
-    cluster_lab = np.load('cluster_lab.npy')
 
-    # plt.imshow(cluster_img[0].reshape((28,28)))
-    # plt.show()
-    # print(cluster_img.shape)
+    # Load data 
+    template_img, template_labels, test_img, test_labels = load_data(N_TRAIN, N_TEST)
+    
+    # Run the clustering once, saves data into files
+    #template_img, template_labels = clustering(train_img,train_labels,64)
+    
+    # If already clustered, load the files instead of running the clustering
+    #template_img = np.load('cluster_img.npy')
+    #template_labels = np.load('cluster_lab.npy')
 
-    nearest_neighbor_classifier(cluster_img, cluster_lab, test_img, test_labels, 640, N_TEST)
-    # clustering(train_img,64)
-
+    # Run nearest neighbor classifier
+    nearest_neighbor_classifier(template_img, template_labels, test_img, test_labels, template_img.shape[0], N_TEST)
     return
 
 if __name__=='__main__':
